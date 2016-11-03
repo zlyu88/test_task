04ba5f22cba341076e880d586cbefc14e5b2e8fa
zlyu88 <zlyu88@gmail.com>
2016-11-03 15:48:04 +0200

    Adding tags

--- blog/forms.py
+++ blog/forms.py
@@ -2,31 +2,19 @@ from django import forms
 
 
 class LogIn(forms.Form):
-<<<<<<< HEAD
     user_name = forms.CharField(label='Your name', max_length=50)
-=======
-    user_name = forms.CharField(label='Your name', max_length=20)
->>>>>>> 4e6d4b6b6ba00630ed7df82a6b542a3c9de17484
     password = forms.CharField(label='Password', max_length=32)
 
 
 class SignUp(forms.Form):
-<<<<<<< HEAD
     user_name = forms.CharField(label='Your name', max_length=50)
     email = forms.EmailField()
     password = forms.CharField(label='Password', max_length=32)
     image = forms.ImageField(required=False, label='Select a file')
-=======
-    user_name = forms.CharField(label='Your name', max_length=20)
-    email = forms.EmailField()
-    password = forms.CharField(label='Password', max_length=32)
-    picture = forms.ImageField(required=False)
->>>>>>> 4e6d4b6b6ba00630ed7df82a6b542a3c9de17484
 
 
 class PostAdd(forms.Form):
     title = forms.CharField(label='title', max_length=50)
-<<<<<<< HEAD
     text = forms.CharField(label='text', widget=forms.Textarea)
     picture = forms.ImageField(required=False, label='Select a file')
 
@@ -45,8 +33,3 @@ class PostEdit(PostAdd):
 
 class TagAdd(forms.Form):
     tag = forms.CharField(label='title', max_length=20)
-
-=======
-    text = forms.CharField(label='text', max_length=500)
-    picture = forms.ImageField(required=False)
->>>>>>> 4e6d4b6b6ba00630ed7df82a6b542a3c9de17484
--- blog/urls.py
+++ blog/urls.py
@@ -6,26 +6,18 @@ urlpatterns = [
 
     url(r'^log_in/$', views.log_in, name='log_in'),
     url(r'^sign_up/$', views.sign_up, name='sign_up'),
-<<<<<<< HEAD
     url(r'^log_out/$', views.log_out, name='log_out'),
-    # url(r'^post_page(?P<user_id>[0-9]+)/$', views.post_page, name='post_page'),
-    url(r'^post_page/(?P<pid>\d+)/$', views.post_page, name='post_page'),
-    url(r'^post_delete/(?P<pid>\d+)/$', views.post_delete, name='post_delete'),
-    url(r'^test/$', views.test, name='test'),
 
+    url(r'^user_page/$', views.user_page, name='user_page'),
+
+    url(r'^post_add/$', views.post_add, name='post_add'),
+    url(r'^post_page/(?P<pid>\d+)/$', views.post_page, name='post_page'),
     url(r'^post_edit/(?P<pid>\d+)/$', views.post_edit, name='post_edit'),
+    url(r'^post_delete/(?P<pid>\d+)/$', views.post_delete, name='post_delete'),
+
     url(r'^tag_add/(?P<pid>\d+)/$', views.tag_add, name='tag_add'),
     url(r'^tag_posts/(?P<tid>\d+)/$', views.tag_posts, name='tag_posts'),
-=======
-    url(r'^$', views.index, name='log_out'),
-    url(r'^post_add/$', views.post_add, name='post_add'),
-    url(r'^post_page/$', views.post_page, name='post_page'),
 
 
->>>>>>> 4e6d4b6b6ba00630ed7df82a6b542a3c9de17484
-    # url(r'^user_profile/$', views.user_profile, name='user_profile'),
-    # url(r'^user_(?P<user_id>[0-9]+)/$', views.user_, name='user_'),
-
-    url(r'^post_add/$', views.post_add, name='post_add'),
-    url(r'^user_page/$', views.user_page, name='user_page'),
+    url(r'^test/$', views.test, name='test'),
 ]
