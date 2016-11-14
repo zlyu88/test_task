59d53be6ae58e21649c58c4404fd26b309b7a4c7
zlyu88 <zlyu88@gmail.com>
2016-11-14 21:21:54 +0200

    files for heroku

--- task/settings.py
+++ task/settings.py
@@ -152,5 +152,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
