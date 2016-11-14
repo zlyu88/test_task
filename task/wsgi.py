9825308416d3d16541462e441b86d83feb3cb99a
zlyu88 <zlyu88@gmail.com>
2016-11-14 21:40:36 +0200

    files for heroku

--- task/wsgi.py
+++ task/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task.settings")
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
 
 from whitenoise.django import DjangoWhiteNoise
 
